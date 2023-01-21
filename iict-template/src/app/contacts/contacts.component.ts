7e5960f17f62817103d9595f37438a27d031a91f
Lalaantika Sharma <sharma.lalaantika@gmail.com>
2023-01-21 10:45:25 -0500

    Contact us page working with the alias contact_us@iiconsult.tech

--- iict-template/src/app/contacts/contacts.component.ts
+++ iict-template/src/app/contacts/contacts.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import {TranslateService} from '@ngx-translate/core';
+import { AppService } from '../services/app.service';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-contacts',
@@ -8,10 +11,32 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
+   FormData: FormGroup;
 
-  constructor(public translate : TranslateService) { }
+  constructor(public translate : TranslateService, private builder: FormBuilder,  private contact: ContactService, private appService: AppService) { }
 
   ngOnInit(): void {
+
+  this.FormData = this.builder.group({
+    Fullname: new FormControl('', [Validators.required]),
+    Email: new FormControl('', [Validators.required, Validators.email]),
+    Comment: new FormControl('', [Validators.required])
+  })
   }
 
+  returnText(key : string, value : string){
+    return this.appService.returnText(key, value);
+  }
+
+  onSubmit(FormData: any) {
+    console.log(FormData)
+    this.contact.PostMessage(FormData)
+    .subscribe(response => {
+    location.href = 'https://mailthis.to/confirm'
+    console.log(response)
+    }, error => {
+    console.warn(error.responseText)
+    console.log({ error })
+    })
+}
 }
