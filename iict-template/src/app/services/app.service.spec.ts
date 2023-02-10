a7e57f058281b8fe27b31f7eb3d28bb2b493be7b
Gervan Nantel <gervann@yahoo.ca>
2023-02-10 10:48:45 -0500

    Completed the updates to the formatting of the CTA section

--- iict-template/src/app/services/app.service.spec.ts
+++ iict-template/src/app/services/app.service.spec.ts
@@ -3,7 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { AppService } from './app.service';
 
 describe('AppService', () => {
-  let service: AppServiceService;
+  let service: AppService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
