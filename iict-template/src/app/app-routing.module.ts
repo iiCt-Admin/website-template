e4936ae1ca7732e5135b22306a991674ccdf7a90
Gervan Nantel <gervann@yahoo.ca>
2023-02-28 06:39:40 -0500

    Updates to home page, organized routing page into order, updated navbar & sidebar to match routing page updates. Added JS code into assets

--- iict-template/src/app/app-routing.module.ts
+++ iict-template/src/app/app-routing.module.ts
@@ -1,40 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FaqComponent } from './faq/faq.component';
-import { LegalComponent } from './legal/legal.component';
-import { HomeComponent } from './home/home.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { TeamsComponent } from './teams/teams.component';
-import { AdminComponent } from './admin/admin.component';
-import { SignupComponent } from './signup/signup.component';
 import { AboutComponent } from './about/about.component';
-import { NewsComponent } from './news/news.component';
+import { AdminComponent } from './admin/admin.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FaqComponent } from './faq/faq.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { LegalComponent } from './legal/legal.component';
+import { NewsComponent } from './news/news.component';
 import { SigninComponent } from './signin/signin.component';
-
+import { SignupComponent } from './signup/signup.component';
+import { TeamsComponent } from './teams/teams.component';
 
 const routes: Routes = [
-
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'admin', component: AdminComponent },
-	{ path: 'contacts', component: ContactsComponent },
-  { path: 'team', component: TeamsComponent },
-  { path: 'legal', component: LegalComponent },
-  { path: 'faq', component: FaqComponent },
-  { path: 'signup', component: SignupComponent },
   { path: 'about', component: AboutComponent},
-  { path: 'news', component: NewsComponent},
+  { path: 'admin', component: AdminComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'faq', component: FaqComponent },
   { path: 'gallery', component: GalleryComponent},
-  { path:'signin',component:SigninComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'legal', component: LegalComponent },
+	{ path: 'news', component: NewsComponent},
+  { path: 'signin',component:SigninComponent},
+  { path: 'signup', component: SignupComponent },
+  { path: 'teams', component: TeamsComponent }
+
   // { path: 'account', component: AccountComponent },
   // { path: 'self-manager-dashboard', component: SelfmanagerPageComponent },
   // { path: 'employee-dashboard', component: EmployeePageComponent },
-  // { path: 'admin', component: AdminComponent },
   // { path : 'add-employee' , component: AddEmployeeComponent },
-
   // { path: '**', component: PageNotFoundComponent }
-
 ];
 
 @NgModule({
