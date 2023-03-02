f5fa49ef20d35301abb45464b39a526cd9a81d53
burningman2k20 <60669658+burningman2k20@users.noreply.github.com>
2023-03-02 17:34:37 -0500

    clean up
    removed unnecessary code and cleaned up method and variable names

--- iict-template/src/app/gallery/gallery.component.ts
+++ iict-template/src/app/gallery/gallery.component.ts
@@ -6,9 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
-	container : any;
-	elements = Array<any>();
-	filterElements = Array<any>();
 	filter = Array<any>();
 
 	portfolio = [{image:"none image",alt:"none alt", title: "no title App 1", category : "App" },
@@ -18,38 +15,26 @@ export class GalleryComponent implements OnInit {
 				{image:"none image",alt:"none alt", title: "no title App 2", category : "App" },
 				{image:"none image",alt:"none alt", title: "no title Web 2", category : "Web"},
 				{image:"none image",alt:"none alt", title: "no title Media 2", category : "Media"}
-				// {image:"none image",alt:"none alt", title: "no title 2", category : "Web"}
 			];
 
   constructor() { }
 
   ngOnInit(): void {
-	  // this.container = document?.getElementById("portfolio");
-	  // if (this.container) this.elements = this.container?.getElementsByTagName("div");
 	  this.filter = this.portfolio;
-	  // alert(this.elements.length);
-	  // alert(this.portfolio.length);
   }
 
-  copyPortfolio(){
-	  for(var i = 0; i < this.portfolio.length; i++){
-	  	// this.filter.
-  	}
-  }
-
-  change(_change : string){
-	  if (_change == "All") {
+  ChangeFilter(filterChange : string){
+	  if (filterChange == "All") {
 		  this.filter = this.portfolio;
 		  return;
 	  }
 	  this.filter = Array<any>();
+	  // Loop thru portfolio array, can be easily changed to loop
+	  // thru data that comes from other sources, such as firebase
 	  for(var i = 0; i < this.portfolio.length; i++){
-		  if (this.portfolio[i].category == _change) this.filter.push(this.portfolio[i]);
-
-		  //console.log(this.elements[i]);
+		  if (this.portfolio[i].category == filterChange) this.filter.push(this.portfolio[i]);
 	  }
 
-	  // alert("change to " + _change);
   }
 
 }
