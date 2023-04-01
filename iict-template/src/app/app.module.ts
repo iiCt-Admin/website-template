490d30ed80f67c9f107b50e497f8e5417266a0ad
Gervan Nantel <gervann@yahoo.ca>
2023-04-01 10:24:54 -0400

    Gervan's updates done in Dave's branch

--- iict-template/src/app/app.module.ts
+++ iict-template/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { FaqComponent } from './faq/faq.component';
 import { FooterComponent } from './footer/footer.component';
 import { GalleryComponent } from './gallery/gallery.component';
-import { PortfolioDetailsComponent } from './portfolio-details/portfolio-details.component';
 import { HeroComponent } from './hero/hero.component';
 import { HomeComponent } from './home/home.component';
 import { LegalComponent } from './legal/legal.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NewsComponent } from './news/news.component';
+import { PortfolioDetailsComponent } from './portfolio-details/portfolio-details.component';
 import { PricingComponent } from './pricing/pricing.component';
 import { ProductsComponent } from './products/products.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
@@ -26,7 +26,6 @@ import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppService } from './services/app.service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -38,10 +37,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { TranslateModule, TranslateLoader} from '@ngx-translate/core';
-
-import * as AOS from 'aos';
 import 'boxicons';
-import { register } from 'swiper/element/bundle';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(httpClient: HttpClient) {
@@ -61,13 +57,14 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
     LegalComponent,
     NavbarComponent,
     NewsComponent,
+    PortfolioDetailsComponent,
     PricingComponent,
     ProductsComponent,
     SidenavComponent,
     SigninComponent,
     SignupComponent,
-    TeamsComponent,
-    PortfolioDetailsComponent
+    TeamsComponent
+
   ],
   imports: [
     BrowserModule,
--- iict-template/src/app/model/user.ts
+++ iict-template/src/app/model/user.ts
@@ -1,4 +1,4 @@
-export interface User {
+export interface UserSettings {
     user_companyId: string;
     user_firstname: string;
     user_investor: boolean;
@@ -6,4 +6,15 @@ export interface User {
     user_ofage: boolean;
     user_passcode: string;
     user_userID: string;
+}
+
+export class UserInfo {
+  user_adress_id: string;
+  user_admin: boolean;
+  user_agreement: boolean;
+  user_client: boolean;
+  user_email_id: string;
+  user_employeer: boolean;
+  user_name: string;
+  user_phone_id;
 }
