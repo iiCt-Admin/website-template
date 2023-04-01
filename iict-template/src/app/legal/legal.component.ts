f80b143a0d1f0c4a906b352249a74ba804e94dd8
Gervan Nantel <gervann@yahoo.ca>
2023-04-01 15:38:11 -0400

    Updates to start on legal component updates

--- iict-template/src/app/legal/legal.component.ts
+++ iict-template/src/app/legal/legal.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AppService } from '../services/app.service'
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-legal',
-  templateUrl: './legal.component.html',
-  styleUrls: ['./legal.component.css']
+  selector: 'ngbd-modal-basic',
+  templateUrl: './legal.component.html'
 })
-export class LegalComponent implements OnInit {
 
-  constructor() { }
+export class LegalComponent {
+closeResult = '';
 
-  ngOnInit(): void {
-  }
+constructor(public appService : AppService, private route: ActivatedRoute, private modalService: NgbModal) {}
 
+returnText(key : string, value : string){
+  return this.appService.returnText(key, value);
+}
+
+open(content) {
+	this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+	this.closeResult = `Closed with: ${result}`;
+	}, (reason) => {
+	this.closeResult =
+		`Dismissed ${this.getDismissReason(reason)}`;
+	});
+}
+
+
+
+private getDismissReason(reason: any): string {
+	if (reason === ModalDismissReasons.ESC) {
+	return 'by pressing ESC';
+	} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+	return 'by clicking on a backdrop';
+	} else {
+	return `with: ${reason}`;
+	}
+}
 }
